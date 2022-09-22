9313e1cbb6d579add7bc5e9aa6d5e0bc2e9ad392
AdamTCoop <abednarz@gmail.com>
2022-09-22 18:43:56 +0200

    Commit test 3

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-for x in range(10):
-    print(x)
+lista_zakupow = {
+    'warzywniak':'marchew',
+    'warzywniak':'seler',
+    'warzywniak':'rukola',
+    'piekarnia':'chleb',
+    'piekarnia':'pączek',
+    'piekarnia':'bułki'
+    }
+
+zak = dict(lista_zakupow)
+lz = iter(lista_zakupow.keys())
+lz2 = iter(lista_zakupow.values())
+print(zak.items())
+
+
+print('Poszedłem do sklepu typu',next(lz),'i kupiłem sobie przedmiot typu:',next(lz2))
+print('Poszedłem do sklepu typu',next(lz),'i kupiłem sobie przedmiot typu:',next(lz2))
