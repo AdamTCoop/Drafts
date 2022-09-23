e2c76439db1b180f536b60d0d2af2a18ef93c882
AdamTCoop <abednarz@gmail.com>
2022-09-23 15:04:22 +0200

    Commit Rozwiazanie na 2 wersje

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+print()
+print('-- Rozw 1 --')
+print()
 lista_zakupow = {
-    'warzywniak':'marchew',
-    'warzywniak':'seler',
-    'warzywniak':'rukola',
-    'piekarnia':'chleb',
-    'piekarnia':'pączek',
-    'piekarnia':'bułki'
-    }
+    'warzywniak':['marchew','seler','rukola'],
+    'piekarnia':['chleb','pączek','bułki']
+}
+for sklep, towar in lista_zakupow.items():
+    print('Poszedłem do sklepu typu',sklep.capitalize(),'i kupiłem sobie przedmiot typu:', end = ' ')
+    for i in towar:
+        print(i.capitalize(), end =' ')
+    print()
+print()
+print('-- Rozw 2 --')
+print()
+lz = next(iter(lista_zakupow.keys()))
+lzz = next(iter(lista_zakupow.keys()))
+lz2 = next(iter(lista_zakupow.values()))
+lz22 = next(iter(lista_zakupow.values()))
 
-zak = dict(lista_zakupow)
-lz = iter(lista_zakupow.keys())
-lz2 = iter(lista_zakupow.values())
-print(zak.items())
-
-
-print('Poszedłem do sklepu typu',next(lz),'i kupiłem sobie przedmiot typu:',next(lz2))
-print('Poszedłem do sklepu typu',next(lz),'i kupiłem sobie przedmiot typu:',next(lz2))
+print('Poszedłem do sklepu typu',lz.capitalize(),'i kupiłem sobie przedmiot typu:',[item.capitalize() for item in lz2])
+print('Poszedłem do sklepu typu',lz.capitalize(),'i kupiłem sobie przedmiot typu:',[item.capitalize() for item in lz2])
