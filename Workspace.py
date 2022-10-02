a8d2dc72a179c283b0b9d2c9049723a16d367938
AdamTCoop <abednarz@gmail.com>
2022-10-02 22:20:03 +0200

    Kalkulejtor v 2

--- Workspace.py
+++ Workspace.py
@@ -1,4 +1,17 @@
-"""print("Kalkulator by CooperAT Corporate")
+def dzial_1(x1,x2):
+    return x1 + x2
+def dzial_2(x1,x2):
+    return x1 - x2
+def dzial_3(x1,x2):
+    return x1 * x2
+def dzial_4(x1,x2):
+    return x1 / x2
+def dzial_5(x1):
+    return x1 / x1
+def dzial_6(x1):
+    return x1 * x1
+
+print("Kalkulator by CooperAT Corporate")
 print()
 print('Niniejsze oprogramowanie umożliwia dokonanie arcytrudnych obliczeń przy użyciu jednego z dostępnych działań.')
 print('Wskaż rodzaj działania które chcesz przeprowadzić, posługując się wskazanym numerem działania:')
@@ -15,32 +28,33 @@ n = int(input())
 print()
 print('Zdecydowałeś się na',n)
 print()
-if n == 5 or 6:
+if n == 6:
     print('Wskaż liczbę do obliczenia: ', end=' ')
-    x1 = input(int())
-else:
+    x1 = int(input())
+    print('Wynik działania wynosi: ',dzial_6(x1))
+elif n == 5:
+    print('Wskaż liczbę do obliczenia: ', end=' ')
+    x1 = int(input())
+    print('Wynik działania wynosi: ',dzial_5(x1))
+elif n == 4:
     print('Wskaż dwie liczby do obliczeń: ', end = ' ')
-    x1 = input(int())
-    x2 = input(int())
-"""
-
-def dzial_1(x1,x2):
-    return x1 + x2
-
-def dzial_2(x1,x2):
-    return x1 - x2
-
-
-def dzial_3(x1,x2):
-    return x1 * x2
-
-
-def dzial_4(x1,x2):
-    return x1 / x2
-
-
-def dzial_5(x1):
-    return x1 / x1
-
-def dzial_6(x1):
-    return x1 * x2
+    x1 = int(input())
+    x2 = int(input())
+    print('Wynik działania wynosi: ', dzial_4(x1,x2))
+elif n == 3:
+    print('Wskaż dwie liczby do obliczeń: ', end = ' ')
+    x1 = int(input())
+    x2 = int(input())
+    print('Wynik działania wynosi: ', dzial_3(x1,x2))
+elif n == 2:
+    print('Wskaż dwie liczby do obliczeń: ', end = ' ')
+    x1 = int(input())
+    x2 = int(input())
+    print('Wynik działania wynosi: ', dzial_2(x1,x2))
+elif n == 1:
+    print('Wskaż dwie liczby do obliczeń: ', end = ' ')
+    x1 = int(input())
+    x2 = int(input())
+    print('Wynik działania wynosi: ', dzial_1(x1,x2))
+else:
+    print("Coś poszło nie tak.")
