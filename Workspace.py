8cf3c1bf22479d9c8ed969fe7ca6edbfb8fd8fca
AdamTCoop <abednarz@gmail.com>
2022-10-02 21:11:39 +0200

    Kalkulejtor

--- Workspace.py
+++ Workspace.py
@@ -1,14 +1,46 @@
-with open('names.txt', 'r') as read_file:
-    for line in read_file.read().splitlines():
-        print(line)
+"""print("Kalkulator by CooperAT Corporate")
 print()
-
-nowa_pozycja = 'Luke'
-with open('names.txt', 'a') as write_file:
-        write_file.write(nowa_pozycja)
-
+print('Niniejsze oprogramowanie umożliwia dokonanie arcytrudnych obliczeń przy użyciu jednego z dostępnych działań.')
+print('Wskaż rodzaj działania które chcesz przeprowadzić, posługując się wskazanym numerem działania:')
 print()
+print('1. Dodawanie')
+print('2. Odejmowanie')
+print('3. Mnożenie')
+print('4. Dzielenie')
+print('5. Pierwiastkowanie')
+print('6. Potęgowanie')
+print()
+print('Jakie działanie zamierzasz przeprowadzic? Wskaż numer: ', end = ' ')
+n = int(input())
+print()
+print('Zdecydowałeś się na',n)
+print()
+if n == 5 or 6:
+    print('Wskaż liczbę do obliczenia: ', end=' ')
+    x1 = input(int())
+else:
+    print('Wskaż dwie liczby do obliczeń: ', end = ' ')
+    x1 = input(int())
+    x2 = input(int())
+"""
 
-with open('names.txt', 'r') as read_file:
-    for line in read_file.read().splitlines():
-        print(line)
+def dzial_1(x1,x2):
+    return x1 + x2
+
+def dzial_2(x1,x2):
+    return x1 - x2
+
+
+def dzial_3(x1,x2):
+    return x1 * x2
+
+
+def dzial_4(x1,x2):
+    return x1 / x2
+
+
+def dzial_5(x1):
+    return x1 / x1
+
+def dzial_6(x1):
+    return x1 * x2
