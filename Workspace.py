80ad53c315fd2c8c974196a81af20674ab2ef914
AdamTCoop <abednarz@gmail.com>
2022-10-02 23:12:20 +0200

    Kalkulejtor v 3 - już liczy ale jeszcze nie loguje

--- Workspace.py
+++ Workspace.py
@@ -1,17 +1,28 @@
-def dzial_1(x1,x2):
-    return x1 + x2
-def dzial_2(x1,x2):
-    return x1 - x2
-def dzial_3(x1,x2):
-    return x1 * x2
-def dzial_4(x1,x2):
-    return x1 / x2
-def dzial_5(x1):
-    return x1 / x1
-def dzial_6(x1):
-    return x1 * x1
-
-print("Kalkulator by CooperAT Corporate")
+def licz14(x1,x2):
+    if n == 6:
+        return x1 * x1
+    elif n == 5:
+        import math
+        return math.sqrt(x1)
+    elif n == 4:
+        return x1 / x2
+    elif n == 3:
+        return x1 * x2
+    elif n == 2:
+        return x1 - x2
+    elif n == 1:
+        return x1 + x2
+    else:
+        return print("Coś poszło nie tak.")
+def licz56(x1):
+    if n == 6:
+        return x1 * x1
+    elif n == 5:
+        import math
+        return math.sqrt(x1)
+        return print("Coś poszło nie tak.")
+print()
+print("Kalkulator by CooperAT Corp.")
 print()
 print('Niniejsze oprogramowanie umożliwia dokonanie arcytrudnych obliczeń przy użyciu jednego z dostępnych działań.')
 print('Wskaż rodzaj działania które chcesz przeprowadzić, posługując się wskazanym numerem działania:')
@@ -26,35 +37,18 @@ print()
 print('Jakie działanie zamierzasz przeprowadzic? Wskaż numer: ', end = ' ')
 n = int(input())
 print()
-print('Zdecydowałeś się na',n)
-print()
-if n == 6:
-    print('Wskaż liczbę do obliczenia: ', end=' ')
-    x1 = int(input())
-    print('Wynik działania wynosi: ',dzial_6(x1))
-elif n == 5:
-    print('Wskaż liczbę do obliczenia: ', end=' ')
-    x1 = int(input())
-    print('Wynik działania wynosi: ',dzial_5(x1))
-elif n == 4:
-    print('Wskaż dwie liczby do obliczeń: ', end = ' ')
-    x1 = int(input())
-    x2 = int(input())
-    print('Wynik działania wynosi: ', dzial_4(x1,x2))
-elif n == 3:
-    print('Wskaż dwie liczby do obliczeń: ', end = ' ')
-    x1 = int(input())
-    x2 = int(input())
-    print('Wynik działania wynosi: ', dzial_3(x1,x2))
-elif n == 2:
-    print('Wskaż dwie liczby do obliczeń: ', end = ' ')
-    x1 = int(input())
-    x2 = int(input())
-    print('Wynik działania wynosi: ', dzial_2(x1,x2))
-elif n == 1:
-    print('Wskaż dwie liczby do obliczeń: ', end = ' ')
-    x1 = int(input())
-    x2 = int(input())
-    print('Wynik działania wynosi: ', dzial_1(x1,x2))
+print('Zdecydowałeś się na:',n)
+if n >= 5:
+    print('Wskaż liczbę do obliczenia. Wybór potwierdź klawiszem enter: ', end=' ')
+    x1 = float(input())
+    print('Wskazales liczbe:', x1)
+    print()
+    print('Wynik działania wynosi: ', licz56(x1))
 else:
-    print("Coś poszło nie tak.")
+    print('Wskaż dwie liczby do obliczeń. Wprowadź pierwszą liczbę i potwierdź klawiszem enter: ', end = ' ')
+    x1 = float(input())
+    print('Wskazales liczbe:', x1,' Liczba numer dwa, to:')
+    x2 = float(input())
+    print('Wskazales liczby',x1,' i ',x2)
+    print()
+    print('Wynik działania wynosi: ', licz14(x1, x2))
