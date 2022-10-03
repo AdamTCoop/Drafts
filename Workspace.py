e6995ddfcb9511fb467b84bf5c1e11592ca68f42
AdamTCoop <abednarz@gmail.com>
2022-10-03 21:31:31 +0200

    Kalkulejtor v 3 - liczy, ignoruje literki, troche loguje, ale jeszcze nie wszystko

--- Workspace.py
+++ Workspace.py
@@ -1,10 +1,8 @@
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename="logfile.log")
+
 def licz14(x1,x2):
-    if n == 6:
-        return x1 * x1
-    elif n == 5:
-        import math
-        return math.sqrt(x1)
-    elif n == 4:
+    if n == 4:
         return x1 / x2
     elif n == 3:
         return x1 * x2
@@ -21,39 +19,68 @@ def licz56(x1):
         import math
         return math.sqrt(x1)
         return print("Coś poszło nie tak.")
-print()
-print("Kalkulator")
-print()
-print('Niniejszy kod umożliwia dokonanie arcytrudnych obliczeń przy użyciu jednego z dostępnych działań.')
-print('Wskaż rodzaj działania które chcesz przeprowadzić, posługując się wskazanym numerem działania:')
-print()
-print('1. Dodawanie')
-print('2. Odejmowanie')
-print('3. Mnożenie')
-print('4. Dzielenie')
-print('5. Pierwiastkowanie')
-print('6. Potęgowanie')
-print()
-print('Jakie działanie zamierzasz przeprowadzic? Wskaż numer: ', end = ' ')
-n = int(input())
-print()
-print('Zdecydowałeś się na:',n)
-wynik = 0
-if n >= 5:
-    print('Wskaż liczbę do obliczenia. Wybór potwierdź klawiszem enter: ', end=' ')
-    x1 = float(input())
-    print('Wskazales liczbe:', x1)
-    print()
-    print('Wynik działania wynosi: ', licz56(x1))
-    wynik = (licz56(x1))
-else:
-    print('Wskaż dwie liczby do obliczeń. Wprowadź pierwszą liczbę i potwierdź klawiszem enter: ', end = ' ')
-    x1 = float(input())
-    print('Wskazales liczbe:', x1,' Liczba numer dwa, to:')
-    x2 = float(input())
-    print('Wskazales liczby',x1,' i ',x2)
-    print()
-    print('Wynik działania wynosi: ', licz14(x1,x2))
-    wynik = licz14(x1)
 
-print('W zaokragleniu: ',(round(wynik,2)))
+if __name__ == "__main__":
+    print()
+    print("Kalkulator")
+    logging.info("Kalkulator")
+    print()
+    print('Niniejszy kod umożliwia dokonanie arcytrudnych obliczeń przy użyciu jednego z dostępnych działań.')
+    print('Wskaż rodzaj działania które chcesz przeprowadzić, posługując się wskazanym numerem działania:')
+    print()
+    print('1. Dodawanie')
+    print('2. Odejmowanie')
+    print('3. Mnożenie')
+    print('4. Dzielenie')
+    print('5. Pierwiastkowanie')
+    print('6. Potęgowanie')
+    print()
+    while True:
+        try:
+            n = int(input('Jakie działanie zamierzasz przeprowadzic? Wskaż numer: '))
+        except ValueError:
+            print('Możesz wprowadzić tylko wartości liczbowe od 1 do 6. Spróbuj jeszcze raz.')
+            continue
+        else:
+            break
+    print('Zdecydowałeś się na:', n)
+    wynik = 0
+    if n >= 5:
+        print('Wskaż liczbę do obliczenia. Wybór potwierdź klawiszem enter: ', end=' ')
+        while True:
+            try:
+                x1 = float(input())
+            except ValueError:
+                print('Możesz wprowadzić tylko wartości liczbowe')
+                continue
+            else:
+                break
+        print('Wskazales liczbe:', x1)
+        print()
+        print('Wynik działania wynosi: ', licz56(x1))
+        wynik = (licz56(x1))
+    else:
+        print('Wskaż dwie liczby do obliczeń. Wprowadź pierwszą liczbę i potwierdź klawiszem enter: ', end = ' ')
+        while True:
+            try:
+                x1 = float(input())
+            except ValueError:
+                print('Możesz wprowadzić tylko wartości liczbowe')
+                continue
+            else:
+                break
+        print('Wskazales liczbe:', x1,' Liczba numer dwa, to:')
+        while True:
+            try:
+                x2 = float(input())
+            except ValueError:
+                print('Możesz wprowadzić tylko wartości liczbowe')
+                continue
+            else:
+                break
+        print('Wskazales liczby',x1,' i ',x2)
+        print()
+        print('Wynik działania wynosi: ', licz14(x1,x2))
+        wynik = licz14(x1,x2)
+
+    print('W zaokragleniu: ',(round(wynik,2)))
