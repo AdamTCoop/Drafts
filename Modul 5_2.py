430fdb9fe3e6b01a86ed92eed58506fd3e86a053
AdamTCoop <abednarz@gmail.com>
2022-10-08 18:08:34 +0200

    Zadanie = wizytownik + dlugosc 99% faker grr

--- Modul 5_2.py
+++ Modul 5_2.py
@@ -10,7 +10,12 @@ class BaseContact:
 
     @property
     def contact(self):
-        print('Wybieram numer +48', self.phone, 'i dzwonię do', self.name, self.last_name)
+        print('Wybieram numer +48', self.phone, 'i dzwonię do', self.name, self.last_name, end = ' ')
+
+    @property
+    def label_length(self):
+        wynik = 1 + len(x.name) + len(x.last_name)
+        print(wynik)
 class BusinessContact(BaseContact):
    def __init__(self, position, company_name, company_number , *args, **kwargs):
        super().__init__(*args, **kwargs)
@@ -27,8 +32,7 @@ Lp6 = BusinessContact(name = fake.name(),last_name = fake.last_name(),phone = fa
 Lp7 = BusinessContact(name = fake.name(),last_name = fake.last_name(),phone = fake.phone_number(),email = fake.email(),position = 'President',company_name = 'Żabka',company_number = fake.phone_number)
 Lp8 = BusinessContact(name = fake.name(),last_name = fake.last_name(),phone = fake.phone_number(),email = fake.email(),position = 'President',company_name = 'Żabka',company_number = fake.phone_number)
 
-
 kontakty = (Lp1,Lp2,Lp3,Lp4,Lp5,Lp6,Lp7,Lp8)
 print()
 for x in kontakty:
-    x.contact
+    x.contact, x.label_length
